067a41d2e970feb4dd0814136a70ac27e329f6c2
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2021-07-17 20:25:05 +0200

    Remove some crefs.

--- Tracer/librt/include/geom/Intersect.h
+++ Tracer/librt/include/geom/Intersect.h
@@ -41,7 +41,7 @@ namespace geom {
 
     namespace priv {
 
-      inline real_t quadraticHit(const real_t& a, const real_t& b, const real_t& c,
+      inline real_t quadraticHit(const real_t a, const real_t b, const real_t c,
                                  const real_t epsilon0)
       {
         real_t t1, t2;
