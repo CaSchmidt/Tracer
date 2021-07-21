64ccf4be43e0290af29dac3312a9d590a977e19b
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2021-07-21 21:24:54 +0200

    The Begin of Part II.

---
+++ Tracer/pt/include/pt/Renderer/PathTracer.h
@@ -0,0 +1,55 @@
+/****************************************************************************
+** Copyright (c) 2021, Carsten Schmidt. All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+**
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+**
+** 3. Neither the name of the copyright holder nor the names of its
+**    contributors may be used to endorse or promote products derived from
+**    this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#ifndef PATHTRACER_H
+#define PATHTRACER_H
+
+#include "rt/Renderer/IRenderer.h"
+
+namespace pt {
+
+  class PathTracer : public rt::IRenderer {
+  public:
+    PathTracer(const rt::RenderOptions& options) noexcept;
+    ~PathTracer() noexcept;
+
+    static rt::RendererPtr create(const rt::RenderOptions& options);
+
+  private:
+    PathTracer() noexcept = delete;
+
+    rt::Color radiance(const rt::Ray& ray, const rt::ScenePtr& scene,
+                       const rt::SamplerPtr& sampler, const rt::uint_t depth = 0) const;
+  };
+
+} // namespace pt
+
+#endif // PATHTRACER_H
---
+++ Tracer/pt/include/pt/Scene/Scene.h
@@ -0,0 +1,61 @@
+/****************************************************************************
+** Copyright (c) 2021, Carsten Schmidt. All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+**
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+**
+** 3. Neither the name of the copyright holder nor the names of its
+**    contributors may be used to endorse or promote products derived from
+**    this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "rt/Base/Types.h"
+#include "rt/Scene/IScene.h"
+
+namespace pt {
+
+  class Scene : public rt::IScene {
+  public:
+    Scene() noexcept;
+    ~Scene() noexcept;
+
+    rt::Color backgroundColor() const;
+    void setBackgroundColor(const rt::Color& c);
+
+    static rt::ScenePtr create();
+
+  private:
+    rt::Color _background{0, 0, 0};
+  };
+
+  inline Scene *SCENE(const rt::ScenePtr& p)
+  {
+    return dynamic_cast<Scene*>(p.get());
+  }
+
+} // namespace pt
+
+#endif // SCENE_H
